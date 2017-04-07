e7db6c30e66c2dfc98cac24555f9885157d41dce
lenny.gade <hermes0616@hanmail.net>
2017-04-07 18:28:52 +0900

    commit

--- wtaiWeather.py
+++ wtaiWeather.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, time, requests
+import sys, os, time, requests
 from slackclient import SlackClient
 from geoCode import GeoCode
 from skpWeather import SkpWeather
